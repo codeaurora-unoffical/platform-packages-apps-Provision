fa3a4cad00577400ee3537f63efaeed40e8a0076
juwei <juwei@codeaurora.org>
2016-08-08 14:22:41 +0800

    Set Launchr3 as default Launcher
    Description:
    As SnapLauncher and Launcher3 are all exist in System.
    make Launcher3 as the default Launcher when user first boot up phone.
    
    Change-Id: I46bf6245bf3feab1b3452d862ebd838370ce67ba
    CRs-Fixed: 1022477

--- src/com/android/provision/DefaultActivity.java
+++ src/com/android/provision/DefaultActivity.java
@@ -18,15 +18,21 @@ package com.android.provision;
 
 import android.app.Activity;
 import android.content.ComponentName;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.provider.Settings;
+
+import java.util.List;
 
 /**
  * Application that sets the provisioned bit, like SetupWizard does.
  */
 public class DefaultActivity extends Activity {
-
+    private static final String ORIGINAL_LAUNCHER_PACKAGENAME = "com.android.launcher3";
+    private static final String ORIGINAL_LAUNCHER_CLASSNAME = "com.android.launcher3.Launcher";
     @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -38,11 +44,49 @@ public class DefaultActivity extends Activity {
         // remove this activity from the package manager.
         PackageManager pm = getPackageManager();
         ComponentName name = new ComponentName(this, DefaultActivity.class);
+
         pm.setComponentEnabledSetting(name, PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
                 PackageManager.DONT_KILL_APP);
-
+        //set Launcher3 as the preferred home activity
+        setupDefaultLauncher(pm);
         // terminate the activity.
         finish();
+    }
+
+    private void setupDefaultLauncher(PackageManager pm){
+        Intent queryIntent = new Intent();
+        queryIntent.addCategory(Intent.CATEGORY_HOME);
+        queryIntent.setAction(Intent.ACTION_MAIN);
+
+        List<ResolveInfo> homeActivities = pm.queryIntentActivities(queryIntent, 0);
+        if(homeActivities == null) {
+            return;
+        }
+
+        ComponentName defaultLauncher = new ComponentName(ORIGINAL_LAUNCHER_PACKAGENAME,
+                ORIGINAL_LAUNCHER_CLASSNAME);
+        int activityNum = homeActivities.size();
+        ComponentName[] set = new ComponentName[activityNum];
+        int defaultMatch = -1;
+        for(int i = 0; i < activityNum; i++){
+            ResolveInfo info = homeActivities.get(i);
+            set[i] = new ComponentName(info.activityInfo.packageName, info.activityInfo.name);
+            if(ORIGINAL_LAUNCHER_CLASSNAME.equals(info.activityInfo.name)
+                    && ORIGINAL_LAUNCHER_PACKAGENAME.equals(info.activityInfo.packageName)){
+                defaultMatch = info.match;
+            }
+        }
+
+        //if Launcher3 is not found, do not set anything
+        if(defaultMatch == -1){
+            return;
+        }
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_MAIN);
+        filter.addCategory(Intent.CATEGORY_HOME);
+        filter.addCategory(Intent.CATEGORY_DEFAULT);
+
+        pm.addPreferredActivity(filter, defaultMatch, set, defaultLauncher);
     }
 }
 
